f9e9bb2713e04bf841d1b7d0ae0eb5c8a4551d9c
shirisha reddy <122144371+shirishareddy28@users.noreply.github.com>
2024-08-26 03:27:49 -0400

    Add files via upload

---
+++ pages/Image1.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import styles from "./Image1.module.css";
+
+const Image1: FunctionComponent = () => {
+  return <div className={styles.image} />;
+};
+
+export default Image1;
